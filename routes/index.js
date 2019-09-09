a25345d4c6f00786443e90ce22d1a5b6a88d290d
jerry <n10327622@qut.edu.au>
2019-09-09 12:46:20 +1000

    change style add markers

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+locations = [
+    {
+        coords: {lat: 42.4778, lng: -70.9495}
+    },
+    {
+        coords: {lat: 42.8584, lng: -70.9300}
+    },
+    {
+        coords: {lat: 42.7762, lng: -71.0773}
+    }
+];
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { markers: locations });
     
     // document.addEventListener("DOMContentLoaded", function() {
     //     let mapElement = document.getElementById('map');
