1ac1f76f07933dd3731a3f7f408e184e5edbee0b
jerry <n10327622@qut.edu.au>
2019-09-12 11:24:59 +1000

    form submit handler, ebay query + limit search

--- app.js
+++ app.js
@@ -91,14 +91,14 @@ app.use('/search', search)
 // })
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; 
 
 // create sever
 const httpServer = http.createServer(app);
-httpServer.listen(3000);
+httpServer.listen(8080);
 
 const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(443);
+httpsServer.listen(8443);
 reload(app);
 
 // function
