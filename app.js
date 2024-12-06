d70c45557e003b21a9642cc656e696d69d63f654
dhienf0707 <duchiennguyenf0707@gmail.com>
2024-12-06 11:53:52 +0700

    fix logical operator

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(express.static(__dirname + '/public')); // static files
 
 // only allow google map api request from localhost
 const allowLocalhostOnly = (req, res, next) => {
-    const referer = req.get('Referer'); // Check the Referer header
-    const isLocal = referer && (referer.startsWith('http://localhost') || referer.startsWith('https://mashup-api.onrender.com/'));
+    const referer = req.get('Referer') // Check the Referer header
+    const isLocal = referer ? referer.startsWith('http://localhost') || referer.startsWith('https://mashup-api.onrender.com/') : false
     if (isLocal) {
         next(); // Allow the request
     } else {
